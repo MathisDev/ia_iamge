a2ed944c8eededffe3c1ef60c6ab71d02ecff095
Mathis <75587138+MathisDev@users.noreply.github.com>
2024-06-13 00:30:17 +0200

    Update install_and_run.sh

--- install_and_run.sh
+++ install_and_run.sh
@@ -2,18 +2,4 @@ pip3 install virtualenv
 python3 -m venv sd_env
 source sd_env/bin/activate
 pip3 install torch torchvision torchaudio diffusers transformers ftfy regex tqdm
-echo "from diffusers import StableDiffusionPipeline
-import torch
-
-model_id = "CompVis/stable-diffusion-v1-4"
-device = "cuda" if torch.cuda.is_available() else "cpu"
-
-pipe = StableDiffusionPipeline.from_pretrained(model_id)
-pipe = pipe.to(device)
-
-prompt = "Une magnifique peinture de paysage au coucher du soleil"
-image = pipe(prompt).images[0]
-
-image.save("output.png")
-" >> main.py
 python3 main.py
